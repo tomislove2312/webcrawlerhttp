308c7578f535fc23bdbc06c1d920e63c23d2d455
Tomislav Marakovic <tomislav.marakovic@aol.com>
2023-03-01 16:46:53 +0100

    printReport finished

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { crawlPage } = require(`./crawl.js`);
-
+const { printReport } = require(`./report.js`);
 async function main() {
   if (process.argv.length < 3) {
     console.log("no website provided");
@@ -14,9 +14,7 @@ async function main() {
   console.log(`starting crawl of ${baseURL}`);
   const pages = await crawlPage(baseURL, baseURL, {});
 
-  for (const page of Object.entries(pages)) {
-    console.log(page);
-  }
+  printReport(pages);
 }
 
 main();
---
+++ report.test.js
@@ -0,0 +1,34 @@
+const { sortPages } = require("./report.js");
+const { test, expect } = require("@jest/globals");
+
+test("sortPages 2 pages", () => {
+  const input = {
+    "https://wagslane.dev/path": 1,
+    "https://wagslane.dev": 3,
+  };
+  const actual = sortPages(input);
+  const expected = [
+    ["https://wagslane.dev", 3],
+    ["https://wagslane.dev/path", 1],
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("sortPages 5 pages", () => {
+  const input = {
+    "https://wagslane.dev/path": 1,
+    "https://wagslane.dev": 3,
+    "https://wagslane.dev/path2": 5,
+    "https://wagslane.dev/path3": 2,
+    "https://wagslane.dev/path4": 9,
+  };
+  const actual = sortPages(input);
+  const expected = [
+    ["https://wagslane.dev/path4", 9],
+    ["https://wagslane.dev/path2", 5],
+    ["https://wagslane.dev", 3],
+    ["https://wagslane.dev/path3", 2],
+    ["https://wagslane.dev/path", 1],
+  ];
+  expect(actual).toEqual(expected);
+});
